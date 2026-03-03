cf2a3b2a4c20b34bc15607fdc5d62edfb3f9a884
YongChan Cho <h3236516@gmail.com>
2026-03-03 13:38:47 +0900

    feat: npm setup

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './api/model';
+export * from './api/endpoints/articles/articles';
+export * from './api/endpoints/default/default';
+export { configureApiClient } from './mutator';
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'RawfliTypes',
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['axios', '@tanstack/react-query', 'react', 'react-dom'],
+      output: {
+        globals: {
+          axios: 'axios',
+          '@tanstack/react-query': 'ReactQuery',
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+    }),
+  ],
+});
