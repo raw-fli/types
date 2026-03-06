75117232cb9e72f7b5b6da0233143703b25affda
YongChan Cho <h3236516@gmail.com>
2026-03-06 10:09:10 +0900

    fix: export files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from './api/model';
 export * from './api/endpoints/articles/articles';
-export * from './api/endpoints/default/default';
+export * from './api/endpoints/auth/auth';
+export * from './api/endpoints/aws/aws';
+export * from './api/endpoints/boards/boards';
+export * from './api/endpoints/posts/posts';
 export { configureApiClient } from './mutator';
